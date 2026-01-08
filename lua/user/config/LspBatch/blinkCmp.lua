00db3f1affeca6dba00d7e6d2c804737e692c7e6
nvrush <madharchodambani@gmail.com>
2026-01-08 13:00:23 +0530

    Taking a leave

--- lua/user/config/LspBatch/blinkCmp.lua
+++ lua/user/config/LspBatch/blinkCmp.lua
@@ -47,7 +47,7 @@ require('blink.cmp').setup({
 
     completion = {
         list = {
-            max_items = 200,
+            max_items = 20,
         },
 
         accept = {
@@ -97,7 +97,7 @@ require('blink.cmp').setup({
                     },
 
                     label = {
-                        width = { fill = true, max = 45 },
+                        width = { fill = true, max = 40 },
                         text = function(ctx) return ctx.label .. ctx.label_detail end,
                         highlight = function(ctx)
                             local highlights = {
@@ -126,7 +126,7 @@ require('blink.cmp').setup({
 
         documentation = {
             auto_show = true,
-            auto_show_delay_ms = 0,
+            auto_show_delay_ms = 200,
             treesitter_highlighting = true,
 
             window = {
@@ -151,58 +151,6 @@ require('blink.cmp').setup({
 
     sources = {
         default = { 'lsp', 'path', 'snippets', 'buffer' },
-        providers = {
-
-            -- Use the thesaurus source
-            thesaurus = {
-                name = "blink-cmp-words",
-                module = "blink-cmp-words.thesaurus",
-                -- All available options
-                opts = {
-                    -- A score offset applied to returned items.
-                    -- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
-                    score_offset = 0,
-
-                    -- Default pointers define the lexical relations listed under each definition,
-                    -- see Pointer Symbols below.
-                    -- Default is as below ("antonyms", "similar to" and "also see").
-                    definition_pointers = { "!", "&", "^" },
-
-                    -- The pointers that are considered similar words when using the thesaurus,
-                    -- see Pointer Symbols below.
-                    -- Default is as below ("similar to", "also see" }
-                    similarity_pointers = { "&", "^" },
-
-                    -- The depth of similar words to recurse when collecting synonyms. 1 is similar words,
-                    -- 2 is similar words of similar words, etc. Increasing this may slow results.
-                    similarity_depth = 2,
-                },
-            },
-
-            -- Use the dictionary source
-            dictionary = {
-                name = "blink-cmp-words",
-                module = "blink-cmp-words.dictionary",
-                -- All available options
-                opts = {
-                    -- The number of characters required to trigger completion.
-                    -- Set this higher if completion is slow, 3 is default.
-                    dictionary_search_threshold = 3,
-
-                    -- See above
-                    score_offset = 0,
-
-                    -- See above
-                    definition_pointers = { "!", "&", "^" },
-                },
-            },
-        },
-
-        -- Setup completion by filetype
-        per_filetype = {
-            text = { "dictionary" },
-            markdown = { "thesaurus" },
-        },
     },
 
     keymap = {
--- lua/user/config/LspBatch/lsp.lua
+++ lua/user/config/LspBatch/lsp.lua
@@ -2,59 +2,6 @@
 -- ====================
 require('luasnip.loaders.from_vscode').lazy_load()
 
--- ====================
--- 2. LSP CAPABILITIES FOR CMP
--- ====================
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- Enable maximum snippet support
-capabilities.textDocument.completion.completionItem = {
-    snippetSupport = true,
-    resolveSupport = {
-        properties = {
-            'documentation',
-            'detail',
-            'additionalTextEdits',
-        }
-    },
-    insertReplaceSupport = true,
-    labelDetailsSupport = true,
-    commitCharactersSupport = true,
-    documentationFormat = { 'markdown', 'plaintext' },
-    deprecatedSupport = true,
-    preselectSupport = true,
-}
-
-
-
---  For blink-cmp Uncomment this :
-local blink_capabilities = require('blink.cmp').get_lsp_capabilities()
-
--- Enable maximum snippet support
-blink_capabilities.textDocument.completion.completionItem = {
-    snippetSupport = true,
-    resolveSupport = {
-        properties = {
-            'documentation',
-            'detail',
-            'additionalTextEdits',
-        }
-    },
-    insertReplaceSupport = true,
-    labelDetailsSupport = true,
-    commitCharactersSupport = true,
-    documentationFormat = { 'markdown', 'plaintext' },
-    deprecatedSupport = true,
-    preselectSupport = true,
-}
-
--- vim.lsp.handlers["textDocument/signatureHelp"] = function() end
--- vim.lsp.handlers["textDocument/documentHighlight"] = function() end
-
--- local servers = {
---     --  Don't add anything here go to lua/user/config/LspConfig/ & make a file there or edit existing
--- }
--- vim.lsp.enable(vim.tbl_keys(servers))
 
 
 vim.lsp.handlers["textDocument/documentHighlight"] = function() end
